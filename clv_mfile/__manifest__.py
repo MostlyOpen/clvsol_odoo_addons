452db780c09fa2fcb1e0fd9614f93aa660b639bd
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-05 15:53:03 -0300

    [IMP] clv_mfile (ADD global_tag)

--- clv_mfile/__manifest__.py
+++ clv_mfile/__manifest__.py
@@ -22,7 +22,7 @@
         'views/mfile_category_view.xml',
         'views/mfile_marker_view.xml',
         'views/mfile_format_view.xml',
-        # 'views/global_tag_view.xml',
+        'views/global_tag_view.xml',
         # # 'views/mfile_annotation_view.xml',
         # 'views/mfile_kanban_view.xml',
         'views/mfile_menu_view.xml',

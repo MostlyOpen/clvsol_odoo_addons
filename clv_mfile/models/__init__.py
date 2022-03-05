67c7c6702a186905490d3b54ca7fa97f4c7b929b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-05 13:40:44 -0300

    [IMP] clv_mfile (Adapt to branch 15.0_dev)

--- clv_mfile/models/__init__.py
+++ clv_mfile/models/__init__.py
@@ -3,10 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import mfile
-from . import mfile_category
-from . import mfile_marker
-from . import global_tag
-# from . import mfile_annotation
-from . import mfile_kanban
-from . import mfile_log
-from . import mfile_format
+# from . import mfile_category
+# from . import mfile_marker
+# from . import global_tag
+# # from . import mfile_annotation
+# from . import mfile_kanban
+# from . import mfile_log
+# from . import mfile_format

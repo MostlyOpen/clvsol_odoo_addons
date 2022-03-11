bb69006a9099d47425bb2aa580d5acfb9c2f5664
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-10 21:09:22 -0300

    [ADD] clv_partner_entity (Copied from branch 14.0)

---
+++ clv_partner_entity/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Partner Entity',
+    'summary': 'Partner Entity Module used by CLVsol Solutions.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'contacts',
+        'base_address_city',
+        'base_address_extended',
+        'clv_base',
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+        # 'views/abstract_partner_entity_view.xml',
+        'views/base_address_city_menu_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
